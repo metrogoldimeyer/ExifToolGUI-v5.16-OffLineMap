93e710548ad1bb6483311b0db3d1df53d9c85301
maura3g <44524325+metrogoldimeyer@users.noreply.github.com>
2024-03-28 15:46:45 +0100

    Add files via upload

--- apps/photos/ExifToolGUI/maps-api-v3/js.js
+++ apps/photos/ExifToolGUI/maps-api-v3/js.js
@@ -106,32 +106,37 @@ function DownloadUrlNavigator4_OLD (url, callback) {
  *       .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)
  *
  */
-google.maps.PlaceSearch = (function (address, callbak) {
+google.maps.PlaceSearch = (function (address, callback) {
 
 	if (isEmptyOrSpaces(address)) {
 		return !0;
 	}
 
 	var url = "https://nominatim.openstreetmap.org/search?format=json&limit=3&q=" + encodeURIComponent(address);
-
+	
 	var ver = navigator.appVersion, ua  = navigator.userAgent, regex;
 	if (contains(ver,"Navigator4.0")) {					// ExifToolGUI v5.16.0.0 Navigator
 		alert("NavigatorVersion:\n" + ver + "\n\nuserAgent:\n" + ua);
-		DownloadUrlNavigator4(url, callbak);			// not working with Navigator4.0 of ExifToolGUI v5.16.0.0
-		// ec.DownloadUrl(url, callbak, false, false);	// not working with Navigator4.0 of ExifToolGUI v5.16.0.0
-		// PI(url, callbak, false, false);				// not working with Navigator4.0 of ExifToolGUI v5.16.0.0
+
+		DownloadUrlNavigator4(url, callback);			// not working with Navigator4.0 of ExifToolGUI v5.16.0.0
+		// ec.DownloadUrl(url, callback, false, false);	// not working with Navigator4.0 of ExifToolGUI v5.16.0.0
+		// PI(url, callback, false, false);				// not working with Navigator4.0 of ExifToolGUI v5.16.0.0
+		// fetchUrl(url, callback);						// Error script with Navigator4.0 of ExifToolGUI v5.16.0.0
 	}
 
 	if ( isValidLatAndLong(address) ) {
 		var loc=address.split(',');
-		callbak(loc[0],loc[1]); // GotoLocation(lat,lon);
+		callback(loc[0],loc[1]); // GotoLocation(lat,lon);
 	} else {
 		alert("GotoAddress: "+address);
-		DownloadUrl(url, callbak);						// working with Chome Windows 7 - ExifToolGUI-maps-api-v3-1g.html
-		// ec.aa(url, true, false, true);				// working with Chome Windows 7 - ExifToolGUI-maps-api-v3-1g.html
-		// ec.DownloadUrl(url, callbak, false, false);	// working with Chome Windows 7 - ExifToolGUI-maps-api-v3-1g.html
-		// PI(url, callbak, false, false);				// working with Chome Windows 7 - ExifToolGUI-maps-api-v3-1g.html
 
+		DownloadUrl(url, callback);						// working with Chome Windows 7 - ExifToolGUI-maps-api-v3-1g.html
+		// ec.aa(url, true, false, true);				// working with Chome Windows 7 - ExifToolGUI-maps-api-v3-1g.html
+		// ec.DownloadUrl(url, callback, false, false);	// working with Chome Windows 7 - ExifToolGUI-maps-api-v3-1g.html
+		// PI(url, callback, false, false);				// working with Chome Windows 7 - ExifToolGUI-maps-api-v3-1g.html
+		// fetchUrl(url, callback);						// working with Chome Windows 7 - ExifToolGUI-maps-api-v3-1g.html
+
+		// getScript(url+"&callback=DownloadUrlScript");
 	}
 });
 
@@ -154,7 +159,7 @@ var ec = {
 	 * DownloadUrl: ec.aa -> ec.DownloadUrl(url, callback, method (false -> GET | true -> POST), ? -> "Content-Type");
 	 *	a: url
 	 *  b: callback
-	 *  c: 
+	 *  c:
 	 */
 	//aa: function(a, b, c, e) {
 	DownloadUrl: function(url, callback, c, e) {
@@ -285,7 +290,7 @@ apiLoad([0.009999999776482582, [
 
 ["https://mts0.google.com/vt?lyrs=m@275000000\u0026src=api\u0026hl=es-ES\u0026",
  "https://mts1.google.com/vt?lyrs=m@275000000\u0026src=api\u0026hl=es-ES\u0026"]],
-	
+
 [["https://mt0.google.com/vt/lyrs=s,h\u0026hl=en\u0026",
   "https://mt1.google.com/vt/lyrs=s,h\u0026hl=en\u0026"], null, null, null, 1, "158",
 
@@ -318,29 +323,25 @@ apiLoad([0.009999999776482582, [
 [["https://mts0.googleapis.com/mapslt/loom?hl=es-ES\u0026", "https://mts1.googleapis.com/mapslt/loom?hl=es-ES\u0026"]],
 
 [["https://mts0.googleapis.com/mapslt?hl=es-ES\u0026", "https://mts1.googleapis.com/mapslt?hl=es-ES\u0026"]],
-	
+
 [["https://mts0.googleapis.com/mapslt/ft?hl=es-ES\u0026", "https://mts1.googleapis.com/mapslt/ft?hl=es-ES\u0026"]],
-	
+
 [["https://mts0.googleapis.com/mapslt/loom?hl=es-ES\u0026", "https://mts1.googleapis.com/mapslt/loom?hl=es-ES\u0026"]]],
 
 
-["es-ES", "ES", null, 0, null, null, "maps-api-v3/mapfiles/", "https://csi.gstatic.com",
-
-null, null, null, "https://maps.google.com"],
-
+["es-ES", "ES", null, 0, null, null, "maps-api-v3/mapfiles/", "https://csi.gstatic.com", null, null, null, "https://maps.google.com"],
 
 ["maps-api-v3/api/js/9/20/intl/es_ALL", "3.9.19"], [4290248692], 1, null, null, null, null, null, "", null, null, 1,
-			
+
 //"https://khms.googleapis.com/mz?v=158\u0026", null, "https://earthbuilder.googleapis.com", "https://earthbuilder.googleapis.com", null,
 // "https://khms.googleapis.com/mz?v=158\u0026", "KEY",
 "https://khms.googleapis.com/mz?v=158\u0026", null,
-// "https://khms.googleapis.com/mz?v=158\u0026", "",
-	 
+
  "https://earthbuilder.googleapis.com", "https://earthbuilder.googleapis.com", null,
  "https://mts.googleapis.com/vt/icon",
 
 [["https://mts0.googleapis.com/vt", "https://mts1.googleapis.com/vt"],
-	
+
 [null, null], [null, [[0, "m", 275000000]], [null, "es-ES", "ES", null, 18, null, null, null, null, null, null, [[47], [37, [["smartmaps"]]]]], 0], [null, [[0, "m", 275000000]], [null, "es-ES", "ES", null, 18, null, null, null, null, null, null, [[47], [37, [["smartmaps"]]]]], 3], [null, [[0, "m", 275000000]], [null, "es-ES", "ES", null, 18, null, null, null, null, null, null, [[50], [37, [["smartmaps"]]]]], 0], [null, [[0, "m", 275000000]], [null, "es-ES", "ES", null, 18, null, null, null, null, null, null, [[50], [37, [["smartmaps"]]]]], 3], [null, [[4, "t", 132], [0, "r", 132000000]], [null, "es-ES", "ES", null, 18, null, null, null, null, null, null, [[63], [37, [["smartmaps"]]]]], 0], [null, [[4, "t", 132], [0, "r", 132000000]], [null, "es-ES", "ES", null, 18, null, null, null, null, null, null, [[63], [37, [["smartmaps"]]]]], 3], [null, null, [null, "es-ES", "ES", null, 18], 0], [null, null, [null, "es-ES", "ES", null, 18], 3], [null, null, [null, "es-ES", "ES", null, 18], 6], [null, null, [null, "es-ES", "ES", null, 18], 0],
 
 ["https://mts0.google.com/vt", "https://mts1.google.com/vt"], "/maps/vt", 275000000, 132], 2, 500,
@@ -352,8 +353,7 @@ null, null, null, "https://maps.google.com"],
 
 var loadScriptTime = (new Date).getTime();
 
-
-getScript("maps-api-v3/api/js/9/20/intl/es_ALL/jquery.min.js");
+// getScript("maps-api-v3/api/js/9/20/intl/es_ALL/jquery.min.js");
 
 var Script="maps-api-v3/api/js/9/20/intl/es_ALL/main.js";
 getScript(Script);
